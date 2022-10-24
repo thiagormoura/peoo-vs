395ea6d32ef2e194ae2487dd941ec5d895ff0e3f
João Thiago Rodrigues de Moura <102044034+thiagormoura@users.noreply.github.com>
2022-10-24 16:01:36 -0300

    Add files via upload

---
+++ questao1-lista4/questao1-lista4/Abastecimento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace questao1_lista4
+{
+    class Abastecimento
+    {
+        private double ValorLitro;
+        private double ValorPago;
+        private double KmRodados;
+        public void SetValorLitro(double ValorLitro)
+        {
+            if (ValorLitro > 0) this.ValorLitro = ValorLitro;
+        }
+        public void SetValorPago(double ValorPago)
+        {
+            if (ValorPago > 0) this.ValorPago = ValorPago;
+        }
+        public void SetKmRodados(double KmRodados)
+        {
+            if (KmRodados > 0) this.KmRodados = KmRodados;
+        }
+        public double MediaKmLitro()
+        {
+            double numLitros = ValorPago / ValorLitro ;
+            return KmRodados / numLitros;
+        }
+        public double MediaReaisKm()
+        {
+            return ValorPago / KmRodados;
+        }
+    }
+}
---
+++ questao1-lista4/questao1-lista4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace questao1_lista4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void KmRodados_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento obj = new Abastecimento();
+            obj.SetKmRodados(double.Parse(KmRodados.Text));
+            obj.SetValorPago(double.Parse(ValorPago.Text));
+            obj.SetValorLitro(double.Parse(ValorLitro.Text));
+            KmLitro.Text = obj.MediaKmLitro().ToString("0.00");
+            ValorKm.Text = obj.MediaReaisKm().ToString("0.00");
+        }
+    }
+}
