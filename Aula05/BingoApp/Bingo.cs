05ca061148833d51b4d23299f85bd1ab6b693c56
thiago.joao@Escolar.ifrn.edu.br <thiago.joao@Escolar.ifrn.edu.br>
2022-11-11 16:02:40 -0300

    bingo-aula05

---
+++ Aula05/BingoApp/Bingo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoApp
+{
+    class Bingo
+    {
+        private int numBolas;
+        private int[] numeros;
+        private int k;
+        public void Iniciar(int numBolas)
+        {
+            if (numBolas >= 10 && numBolas <= 100)
+                this.numBolas = numBolas;
+            else
+                this.numBolas = 50;
+            numeros = new int[numBolas];
+            k = 0;
+        }
+        public int Sortear()
+        {
+            if (k == numBolas) return -1;
+            Random r = new Random();
+            int n = r.Next(1, numBolas + 1);
+            int pos = Array.IndexOf(numeros, n);
+            while (pos != -1)
+            {
+                n = r.Next(1, numBolas + 1);
+                pos = Array.IndexOf(numeros, n);
+            }
+            numeros[k] = n;
+            k++;
+            return n;
+        }
+        public int[] Sorteados()
+        {
+            int[] vetor = new int[k];
+            Array.Copy(numeros, vetor, k);
+            Array.Sort(vetor);
+            return vetor;
+        }
+
+    }
+}
---
+++ Aula05/BingoApp/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BingoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtNumBolas.Text = slider.Value.ToString();
+        }
+
+        private Bingo b;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b = new Bingo();
+            b.Iniciar(int.Parse(txtNumBolas.Text));
+            btnIniciar.IsEnabled = false;
+            btnSortear.IsEnabled = true;
+            txtNum.Clear();
+            txtSorteados.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int x = b.Sortear();
+            if (x == -1)
+            {
+                txtNum.Text = "Fim";
+                btnIniciar.IsEnabled = true;
+                btnSortear.IsEnabled = false;
+            }
+            else
+            {
+                txtNum.Text = x.ToString();
+                string s = "";
+                foreach (int i in b.Sorteados())
+                    s = s + i + " - ";
+                txtSorteados.Text = s;
+            }
+        }
+    }
+}
