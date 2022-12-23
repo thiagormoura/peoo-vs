262775be4d299c1c4943a102c2ffc412598600a8
20211011110011 <thiago.joao@escolar.ifrn.edu.br>
2022-12-23 17:46:46 -0300

    AppEscola

---
+++ EscolaApp/EscolaApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EscolaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Turma_Click(object sender, RoutedEventArgs e)
+        {
+            janelaturma w = new janelaturma();
+            w.ShowDialog();
+        }
+
+        private void Aluno_Click(object sender, RoutedEventArgs e)
+        {
+            janelaaluno w = new janelaaluno();
+            w.ShowDialog();
+        }
+
+        private void Matricula_Click(object sender, RoutedEventArgs e)
+        {
+            janelamatricula w = new janelamatricula();
+            w.ShowDialog();
+        }
+
+        private void Diario_Click(object sender, RoutedEventArgs e)
+        {
+            janeladiario w = new janeladiario();
+            w.ShowDialog();
+        }
+    }
+}
---
+++ EscolaApp/EscolaApp/Nturma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+     static class Nturma
+    {
+        // private Turma[] turmas = new Turma[10];
+        private static List<Turma> turmas = new List<Turma>();
+        private  static void Inserir(Turma t)
+        {
+            turmas.Add(t);
+        }
+        private static List<Turma > Listar()
+        {
+            return turmas;
+        }
+    }
+}
